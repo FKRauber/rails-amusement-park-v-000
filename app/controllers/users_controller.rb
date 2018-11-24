21472872a2595f0f3f56bb074e23621d29c11a9c
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-24 19:29:46 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,8 @@ class UsersController < ApplicationController
   def edit
   end
   def update
-    if @user = User.update(user_params)
-      redirect_to @user, notice: "User was successfully updated."
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "User was successfully updated."
     else
       render :edit
     end
