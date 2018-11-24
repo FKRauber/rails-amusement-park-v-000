8ce1fa806153ccbe951b545ab75dd19510c7b56c
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-24 20:12:32 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+
   resources :users
   resources :attractions
 
-  root "static_pages#home"
   get "/signin", to: "sessions#new"
   post "/signin", to: 'sessions#create'
   post "/sessions/create", to: "sessions#create"
