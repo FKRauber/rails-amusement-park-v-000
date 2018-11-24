d8574413c96969a5d56453ee433904dd6752990b
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-24 20:33:57 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,7 +1,7 @@
 class RidesController < ApplicationController
   def new
     @ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
-    @message = @ride.go_ride
+    @message = @ride.take_ride
     redirect_to user_path(@ride.user, message: @message)
   end
 end
